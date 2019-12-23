9f54eb737e10a1d0f671270a1a00f8f5f10f2e6c
bgolda <41947259+bgolda@users.noreply.github.com>
2019-12-23 08:53:24 +0100

    Update lab_5.py

--- lab_5.py
+++ lab_5.py
@@ -29,3 +29,9 @@ cv2.imshow('Bitwise Or', bitwise_or)
 if cv2.waitKey(0) & 0xff == 27:  
 	cv2.destroyAllWindows()
 	
+bitwise_xor = cv2.bitwise_xor(image1, image2, mask = None)
+cv2.imshow('Bitwise XOr', bitwise_xor)
+
+if cv2.waitKey(0) & 0xff == 27:  
+	cv2.destroyAllWindows()
+	
