31378bfde7e85188338c54e31ff5b9c13ddf93de
GoldBar0 <57587008+GoldBar0@users.noreply.github.com>
2020-02-01 15:33:02 +0100

    tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,10 @@
 import itertools
 
-size = 3
+size = int(input("Please enter the size of the grid: "))
+number = int(input("Please enter the winning number: "))
+numbers = []
+numbers.append(number)
+print(numbers)
 
 def print_board(board):
     for index, cell in enumerate(board, start=1):
@@ -8,7 +12,11 @@ def print_board(board):
 
 def do_turn(board, player):
     position = int(input("Player {}. Enter the position to play: ".format(player))) - 1
-    board[position] = player
+    if board[position] == None:
+        board[position] = player
+    else:
+        print("You're not allowed to do that \n")
+        player = next(players)
 
 def is_draw(board):
     return all (board[position] is not None for position in range(size*size))
@@ -17,10 +25,10 @@ def has_won(board, player):
     def has_combination(combination, symbol):
         return all(board[row*size + col] == symbol for row, col in combination)
     winning_patterns = [
-        lambda row, col: [(row-1, col), (row, col), (row+1, col)] if 0 < row < size-1 else None,
-        lambda row, col: [(row, col-1), (row, col), (row, col+1)] if 0 < col < size-1 else None,
-        lambda row, col: [(row-1, col-1), (row, col), (row+1, col+1)] if 0 < row < size-1 and 0 < col < size-1 else None,
-        lambda row, col: [(row-1, col+1), (row, col), (row+1, col-1)] if 0 < row < size-1 and 0 < col < size-1 else None
+        lambda row, col: [(row-2, col), (row-1, col), (row, col), (row+1, col), (row+2, col)] if 1 < row < size-2 else None,
+        lambda row, col: [(row, col-2), (row, col-1), (row, col), (row, col+1), (row, col+2)] if 1 < col < size-2 else None,
+        lambda row, col: [(row-2, col-2), (row-1, col-1), (row, col), (row+1, col+1), (row+2, col+2)] if 1 < row < size-2 and 1 < col < size-2 else None,
+        lambda row, col: [(row-2, col+2), (row-1, col+1), (row, col), (row+1, col-1), (row+2, col-2)] if 1 < row < size-2 and 1 < col < size-2 else None
     ]
 
     combinations = (pattern(position // size, position % size) for pattern in winning_patterns for position in range (size*size))
